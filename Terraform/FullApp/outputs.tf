1983a3352687e339e47350061046099941a7ef2a
ericsakamoto <eric.sakamoto@gmail.com>
2021-10-26 14:43:35 +0000

    Removed

--- Terraform/FullApp/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.skmt_fullapp_server.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.skmt_fullapp_server.public_ip
-}
--- Terraform/FullApp/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "fullapp_instance_name" {
-  description = "Full App EC2 instance"
-  type        = string
-  default     = "SKMT-FullApp-Server"
-}
